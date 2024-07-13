c0c133afa790f0caef8471d077198c3e35d82bd6
Idham Atta <idhamatta2706@gmail.com>
2024-07-13 23:29:13 +0700

    adding logo and text

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -6,21 +6,39 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
+      backgroundColor: Color.fromARGB(255, 3, 29, 39),
       appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 3, 29, 39),
         title: Text(
           'Rodrik E-Commerce',
         ),
         centerTitle: true,
-        titleTextStyle: TextStyle(fontSize: 16, color: Colors.white),
+        titleTextStyle: TextStyle(
+            fontSize: 16, color: Colors.white, fontWeight: FontWeight.bold),
       ),
-      body: Container(
-        margin: EdgeInsets.all(12),
-        color: Colors.purple,
+      body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text('data'),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              // Untuk Logo
+              child: Image.asset(
+                'lib/images/rodriklogo.png',
+                height: 250,
+              ),
+            ),
+            const SizedBox(height: 48),
+            // Title
+            Text(
+              'Ici Cest Rodrik',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  color: Colors.white),
+            )
+            // Sub Title
+
+            // Button Start Now
           ],
         ),
       ),
