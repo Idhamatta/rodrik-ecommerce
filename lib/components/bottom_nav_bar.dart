5c0e94e664874e1de397e097d3d9fae620d8d51c
Idham Atta <idhamatta2706@gmail.com>
2024-07-14 13:09:23 +0700

    add buttonstyle on navbar

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -7,26 +7,28 @@ class MyBottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: GNav(color: Color(0xFF12ad95), tabs: [
-        GButton(
-          icon: Icons.home_filled,
-          text: 'Shop',
-          textColor: Colors.white,
-          iconActiveColor: Colors.white,
-        ),
-        GButton(
-          icon: Icons.shopping_bag_outlined,
-          text: 'Cart',
-          textColor: Colors.white,
-          iconActiveColor: Colors.white,
-        ),
-        GButton(
-          icon: Icons.wallet,
-          text: 'Payment',
-          textColor: Colors.white,
-          iconActiveColor: Colors.white,
-        ),
-      ]),
+      child: GNav(
+          color: Color(0xFF12ad95),
+          activeColor: Colors.white,
+          tabActiveBorder: Border.all(color: Color.fromARGB(255, 3, 29, 39)),
+          tabBackgroundColor: Color(0xFF006e83),
+          tabs: [
+            GButton(
+              icon: Icons.home_filled,
+              text: 'Shop',
+              textColor: Colors.white,
+            ),
+            GButton(
+              icon: Icons.shopping_bag_outlined,
+              text: 'Cart',
+              textColor: Colors.white,
+            ),
+            GButton(
+              icon: Icons.wallet,
+              text: 'Payment',
+              textColor: Colors.white,
+            ),
+          ]),
     );
   }
 }
