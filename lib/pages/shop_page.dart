3c42fb0b33a7128f464d26befdbc1e0635aa7f5b
Idham Atta <idhamatta2706@gmail.com>
2024-07-15 20:31:57 +0700

    style home_page

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -10,12 +10,49 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Shop',
-        style: TextStyle(
-            color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
-      ),
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(12),
+          margin: EdgeInsets.symmetric(horizontal: 25.0),
+          decoration: BoxDecoration(
+              color: Color(0xFF006e83), borderRadius: BorderRadius.circular(8)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Search',
+                style: TextStyle(color: Color.fromARGB(255, 3, 29, 39)),
+              ),
+              Icon(
+                Icons.search_rounded,
+                color: Color.fromARGB(255, 3, 29, 39),
+              )
+            ],
+          ),
+        ),
+
+        // Message
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0),
+          child: Text(
+            'Ich Bin Ein Berliner, Deutschland Uber Alles',
+            style: TextStyle(
+                color: Color(0xFF12ad95), fontWeight: FontWeight.bold),
+          ),
+        ),
+
+        // Hot Picks
+        const Row(
+          children: [
+            Text(
+              'Hot Picks 🔥',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            )
+          ],
+        )
+      ],
     );
   }
 }
