ed7014fd69e14ddb9aa2159945c16c096a5092f4
Idham Atta <idhamatta2706@gmail.com>
2024-07-25 20:56:13 +0700

    change padding in shoe_tile

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -24,7 +24,7 @@ class ShoeTile extends StatelessWidget {
               )),
           // description
           Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(21.0),
             child: Text(
               shoe.description,
               style:
@@ -33,40 +33,53 @@ class ShoeTile extends StatelessWidget {
             ),
           ),
           // Price + Details
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                children: [
-                  // Shoe Name
-                  Text(
-                    shoe.name,
-                    style: TextStyle(
-                        fontSize: 15,
-                        color: Color(0xFFc1faff),
-                        fontWeight: FontWeight.bold),
-                  ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Shoe Name
+                    Text(
+                      shoe.name,
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Color(0xFFc1faff),
+                          fontWeight: FontWeight.bold),
+                    ),
 
-                  // Price
-                  Text(
-                    shoe.price,
-                    style: TextStyle(
-                        fontSize: 15,
-                        color: Color(0xFFc1faff),
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
+                    const SizedBox(height: 5),
 
-              // Plus Button
-              Container(
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                    color: Color(0xFFc1faff),
-                    borderRadius: BorderRadius.circular(8)),
-                child: Icon(Icons.add_rounded),
-              )
-            ],
+                    // Price
+                    Text(
+                      shoe.price,
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Color(0xFFc1faff),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+
+                // Plus Button
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFc1faff),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        bottomRight: Radius.circular(12),
+                      ),
+                    ),
+                    child: Icon(Icons.add_rounded),
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
